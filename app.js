95ff66109d84f645f8d3d02058f1f581504a7604
csy <csl8804@gmail.com>
2022-03-21 19:26:42 +0900

    init

---
+++ app.js
@@ -0,0 +1,51 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const corsConfig = require('./config/corsConfig.json');
+const logger = require('./lib/logger');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/user');
+const passportConfig = require('./passport');
+
+const app = express();
+passportConfig(); // 패스포트 설정
+logger.info('app start');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
