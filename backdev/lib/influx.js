460d2709094bd368c0a39d1f3bb9a2156a0d86d3
csy <csl8804@gmail.com>
2022-04-19 20:14:50 +0900

    feat: influx

--- backdev/lib/influx.js
+++ backdev/lib/influx.js
@@ -1,27 +1,62 @@
 const Influx = require('influx');
+
 const logger = require('./logger');
 
-const client = new Influx.InfluxDB({
+const influx = new Influx.InfluxDB({
   host: 'localhost',
   database: 'boyeo',
   port: 8086,
 });
 
 // DB 없으면 만들기
-Influx.getDatabaseNames()
-  // eslint-disable-next-line consistent-return
-  .then((names) => {
-    if (!names.include('boyeo')) {
-      return Influx.createDatabase('boyeo');
-    }
-  });
+// Influx.getDatabaseNames()
+//   // eslint-disable-next-line consistent-return
+//   .then((names) => {
+//     if (!names.include('boyeo')) {
+//       return Influx.createDatabase('boyeo');
+//     }
+//   });
+const dao = {
+  create() {
+    return new Promise((resolve, reject) => {
+      influx.query(
+        'CREATE RETENTION POLICY "one_day_onl" ON "boyeo" DURATION 1d REPLICATION 1',
+      ).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        logger.error(err);
+        reject(err);
+      });
+    });
+  },
 
-Influx.query(
-  'select * from mqtt_edukit',
-).catch((err) => {
-  logger.error(err);
-}).then((results) => {
-  results.json(results);
-});
+  select() {
+    return new Promise((resolve, reject) => {
+      influx.query(
+        'SELECT * FROM mqtt_edukit ORDER BY time DESC',
+      ).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        logger.error(err);
+        reject(err);
+      });
+    });
+  },
 
-module.exports = Influx;
+  defect() {
+    return new Promise((resolve, reject) => {
+      influx.query(
+        'SELECT (No1Count - No3Count) FROM "boyeo"',
+        'SELECT (No1Count - No3Count)/No1Count FROM "boyeo"',
+      ).then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        logger.error(err);
+        reject(err);
+      });
+    });
+  },
+
+};
+
+module.exports = dao;
